01bd92bda2d88b805979b05bf570b54d70bfb4fa
prajwel <prajwel.pj@gmail.com>
2019-04-05 12:26:29 +0530

    Add files via upload

---
+++ UL2P multipipes/VIS_multipipes_V.1.6.sh
@@ -0,0 +1,44 @@
+#!/bin/csh
+
+which UVIT_DriverModule
+
+# Random name generation 
+set random_name = "`openssl rand -hex 4`"
+
+# To set local environment variables and what not.
+setenv PFILES "$HOME/multipipe_$random_name/paramfiles"
+setenv GLOG_log_dir "$HOME/multipipe_$random_name/log"
+mkdir -p $PFILES $GLOG_log_dir
+cp -R $AS1/uvit/paramfiles/* $PFILES
+
+#To copy VIS driver to paramfiles.
+cp $HOME/switch_drivers/VIS_UVIT_DriverModule.par $PFILES/UVIT_DriverModule.par
+
+# Run the driver module
+UVIT_DriverModule n LEVL1AS1UVT*tar_V*
+
+# To copy the driver module param file
+cp $PFILES/UVIT_DriverModule.par .
+
+# To remove the temporary log and paramfiles directory.
+rm -rf $HOME/multipipe_$random_name
+
+# To remove unwanted files
+rm -rf output_RAPC*
+rm -rf AUX4*
+rm -rf *_level*
+rm -f *_EDITED_L2
+rm -f *PCONLY*
+rm -f Differences.txt
+rm -f Driver_reference.txt
+rm -f Driver_Total_NUV_SCIENCEDATAFILE.txt
+rm -f xytheta.txt
+rm -f ZeroCentroid.txt
+rm -f core.*
+find . -name 'DataIngest_*' -type d -exec rm -rf {} +
+
+# To remove the L2tars. (IMPORTANT!)
+rm -rf /data/pipeline/prajwel/L2tar/*
+
+
+
